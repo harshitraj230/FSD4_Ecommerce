4446ce65fba7b66e5de1c90c5c70f90767fdc15a
Harshit Raj <harshitraj230@gmail.com>
2023-07-09 16:26:12 +0530

    login and used async await

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -1,5 +1,8 @@
 const {signUpValidator}=require("../Middlewares");
 const authController=require("../Controllers/auth.controller");
+
 module.exports=function(app){
     app.post("/ecomm/api/v1/auth/signup",[signUpValidator.checkDuplicateEmailOrUserName,signUpValidator.checkRolesExists],authController.signup);
+
+    app.post("/ecomm/api/v1/auth/signin",authController.signIn);
 }
