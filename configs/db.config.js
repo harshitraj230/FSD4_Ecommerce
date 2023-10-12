4628b9cc7dbd13b6934f18c48cfd07ebb3e9a991
Harshit Raj <harshitraj230@gmail.com>
2023-10-12 12:11:49 +0530

    added configs for production environment

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    development: {
     HOST : "localhost",
     USER : "root",
     PASSWORD : "root123",
@@ -10,4 +11,31 @@ module.exports = {
     acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
     idle :10000 // maximum time in ms that a connection can be idle before being released
     }
-   }
+   },
+   test: {
+    HOST : "localhost",
+    USER : "root",
+    PASSWORD : "harshit@16",
+    DB : "ecom_test_db",
+    dialect : "mysql",
+    pool :{
+    max :5,
+    min :0,
+    acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
+    idle :10000 // maximum time in ms that a connection can be idle before being released
+    }
+   },
+   production:{
+    HOST : "sql12.freemysqlhosting.net",
+    USER : "sql12652483",
+    PASSWORD : "2plttqqlGa",
+    DB : "sql12652483",
+    dialect : "mysql",
+    pool :{
+    max :5,
+    min :0,
+    acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
+    idle :10000 // maximum time in ms that a connection can be idle before being released
+}
+}
+}
